54030bf9171d494979c064cc4b7a9e471052cebf
Paola Pileño <paola.pileno@seidor.com>
2018-04-04 18:27:07 -0500

    Add test cases

--- test/TennisGameSpec.ts
+++ test/TennisGameSpec.ts
@@ -9,8 +9,20 @@ describe('For a TennisGame', function() {
     g = new TennisGameImpl();
   });
 
-  it('when it has just started, the score should be "love-all"', function() {
-    expect(g.getScore()).toBe('love-all');
+  it('Examples 1. when it has just started, the score should be "love-all"', function() {
+    expect(g.getScore(0,0)).toBe('love-all');
+  });
+
+  it('Examples 2. the first player has won one point and second none, the score is "15-love"', function() {
+    expect(g.getScore(1,0)).toBe('15-love');
+  });
+
+  it('Example 3. the first player is named "A" and has won four points and the second player has won three points.', function() {
+    expect(g.getScore(4,3)).toBe('Advantage A');
+  });
+
+  it('Example 4. the first player is named "A" and has won five points and the second player has won three points, player "A" has won the game.', function() {
+    expect(g.getScore(5,3)).toBe('Game A');
   });
   
 });
