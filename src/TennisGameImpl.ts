febfc137be09535cc7407e6815e1322a5d279a07
Paola Pileño <paola.pileno@seidor.com>
2018-04-04 15:33:23 -0500

    Fisrt try for display score.

--- src/TennisGameImpl.ts
+++ src/TennisGameImpl.ts
@@ -3,10 +3,47 @@
 class TennisGameImpl implements TennisGame {
 
   wonPoint(player: Player) {
-
+    
   }
 
   getScore(): string {
-    return "I don't know?";
+    var pointsA = 3;
+    var pointsB = 5;
+    var score = "";
+
+    if(pointsA == pointsB && pointsA < 3) {
+      score += this.getScoreByPoints(pointsA) + "-all";
+    } else if(pointsA == pointsB && pointsA >= 3) {
+      score = "douce";
+    } else if(pointsA > 3 && pointsB > 3 && Math.abs(pointsA - pointsB) == 1) {
+      score = "Advantage " + this.getHigherPlayer(pointsA, pointsB);
+    } else if(pointsA >= 4 || pointsB >= 4 && Math.abs(pointsA - pointsB) > 2) {
+      score = "Game " + this.getHigherPlayer(pointsA, pointsB);      
+    } else {
+      score = this.getScoreByPoints(pointsA) + "-" + this.getScoreByPoints(pointsB);
+    }
+
+    return score;
+
+  }
+
+  getScoreByPoints(points: number) : string {
+    if(points == 0)
+      return "love";
+    if(points == 1)
+      return "15";
+    if(points == 2)
+      return "30";
+    if(points == 3)
+      return "40"
+    if(points == 4)
+      return "game";    
+  }
+
+  getHigherPlayer(pointsA: number, pointsB: number) : string {
+    if(pointsA > pointsB)
+      return " A";
+    else
+      return " B";
   }
 }
